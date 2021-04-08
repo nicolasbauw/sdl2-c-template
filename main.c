3d57c263118d6eef13317b9d3ff66d91ad9c72e8
Nicolas BAUW <nbauw@hotmail.com>
2021-04-08 18:06:31 +0200

    Fixed scope mistakes

--- main.c
+++ main.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int init(SDL_Window *window);
+SDL_Window* init();
 void quit(SDL_Window *window);
 void loop();
 
 int main(int argc, char* argv[]) {
 
-    SDL_Window *window;                    // Declare a pointer
+    SDL_Window* window;                    // Declare a pointer
 
-    if (init(window) == -1) {
+    if ((window = init()) == NULL) {
         return EXIT_FAILURE;
     };
     
@@ -20,11 +20,11 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
-int init(SDL_Window *window) {
+SDL_Window* init() {
     SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
 
     // Create an application window with the following settings:
-    window = SDL_CreateWindow(
+    return SDL_CreateWindow(
         "An SDL2 window",                  // window title
         SDL_WINDOWPOS_UNDEFINED,           // initial x position
         SDL_WINDOWPOS_UNDEFINED,           // initial y position
@@ -32,18 +32,9 @@ int init(SDL_Window *window) {
         480,                               // height, in pixels
         SDL_WINDOW_SHOWN                   // flags - see below
     );
-
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return -1;
-    }
-
-    return 0;
 }
 
-void quit(SDL_Window *window) {
+void quit(SDL_Window* window) {
     // Close and destroy the window
     SDL_DestroyWindow(window);
 
